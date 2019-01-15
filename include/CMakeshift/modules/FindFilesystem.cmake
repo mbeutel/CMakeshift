253ca94cfc0a5e41802ab82f69e6318c9b0e34da
Moritz Beutel <mbeutel@users.noreply.github.com>
2019-01-15 05:07:23 +0100

    Add find module for `<filesystem>` (needs special link library for GCC)

---
+++ include/CMakeshift/modules/FindFilesystem.cmake
@@ -0,0 +1,31 @@
+
+#.rst:
+# FindFilesystem
+# --------------
+#
+# Define an imported target that represents the features provided by the C++17 <filesystem> header.
+#
+# This will define the following variables::
+#
+#   Filesystem_FOUND - True
+#
+# and the following imported targets::
+#
+#   Filesystem::Filesystem - Imported target that carries linker flags required to use <filesystem> on all platforms.
+
+
+set(Filesystem_FOUND True)
+
+    # Define a target only if none has been defined yet.
+if(NOT TARGET Filesystem::Filesystem)
+    add_library(Filesystem::Filesystem INTERFACE IMPORTED)
+
+    # GCC 8.x implements <filesystem> but currently requires that we link to libstdc++fs when using it.
+    if((CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX) AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9.1)
+        set_property(TARGET Filesystem::Filesystem PROPERTY INTERFACE_LINK_LIBRARIES "stdc++fs")
+    endif()
+
+    if(NOT Filesystem_FIND_QUIETLY)
+        message(STATUS "Found Filesystem (find module at ${CMAKE_CURRENT_LIST_DIR})")
+    endif()
+endif()
