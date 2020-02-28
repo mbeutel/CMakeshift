c1c002f6195bffe37f469e924d42090e96e78908
Moritz Beutel <mbeutel@users.noreply.github.com>
2020-02-28 22:41:45 +0100

    Add "ParallelAlgorithms" find module

--- cmake/CMakeshift/modules/FindFilesystem.cmake
+++ cmake/CMakeshift/modules/FindFilesystem.cmake
@@ -16,11 +16,11 @@
 
 set(Filesystem_FOUND True)
 
-    # Define a target only if none has been defined yet.
 if(NOT TARGET Filesystem::Filesystem)
+    # Define a target only if none has been defined yet.
     add_library(Filesystem::Filesystem INTERFACE IMPORTED)
 
-    # GCC 8.x implements <filesystem> but currently requires that we link to libstdc++fs when using it.
+    # GCC 8's libstdc++ implements <filesystem> but currently requires that we link to libstdc++fs when using it.
     if((CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX) AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9.1)
         set_property(TARGET Filesystem::Filesystem PROPERTY INTERFACE_LINK_LIBRARIES "stdc++fs")
     endif()
