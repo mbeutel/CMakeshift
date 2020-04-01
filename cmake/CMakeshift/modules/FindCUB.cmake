a9003ec090f6687872b2c8b641e2788ccc95d23a
Christoph Klein <codecircuit@gpu-acceleration.com>
2020-04-02 00:40:50 +0200

    Add FindCUB.cmake for the CUB CUDA library (#8, thanks to @codecircuit)

---
+++ cmake/CMakeshift/modules/FindCUB.cmake
@@ -0,0 +1,29 @@
+
+#.rst:
+# FindCUB
+# ----------
+#
+# Find the CUB library
+#
+# This find module will define the imported target::
+#
+#   CUB::CUB - CUB header-only CUDA library
+
+find_path(CUB_INCLUDE_DIR
+    NAMES "cub/cub.cuh"
+    PATH_SUFFIXES "include")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(CUB
+    REQUIRED_VARS CUB_INCLUDE_DIR)
+
+if(CUB_FOUND)
+    # Define a target only if none has been defined yet.
+    if(NOT TARGET CUB::CUB)
+        add_library(CUB::CUB INTERFACE IMPORTED)
+        set_target_properties(CUB::CUB PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${CUB_INCLUDE_DIR}")
+    endif()
+endif()
+
+mark_as_advanced(CUB_INCLUDE_DIR)
